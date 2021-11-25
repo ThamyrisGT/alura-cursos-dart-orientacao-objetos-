18f714acd491881fc0ec62bf5cccab99c121d197
Thamyris <tgthurler@gmail.com>
2021-11-25 10:54:38 -0300

    Métodos com mais de um argumento e refatorando código

--- alura_bank/bin/alura_bank.dart
+++ alura_bank/bin/alura_bank.dart
@@ -2,23 +2,11 @@ void main() {
   ContaCorrente contaCorrente1 = ContaCorrente();
   ContaCorrente contaCorrente2 = ContaCorrente();
   contaCorrente1.titular = 'Amanda';
-  contaCorrente1.titular = 'Thiago';
+  contaCorrente2.titular = 'Thiago';
 
-//   contaCorrente1.deposito(20);
-//   contaCorrente2.deposito(50);
-//   print('Saldo da ${contaCorrente1.titular}:${contaCorrente1.saldo} ');
-//   print('Saldo da ${contaCorrente2.titular}:${contaCorrente2.saldo} ');
-//   contaCorrente1.saque(80.0);
-//   contaCorrente2.saque(130.0);
-//   print('Saldo da ${contaCorrente1.titular}:${contaCorrente1.saldo} ');
-//   print('Saldo da ${contaCorrente2.titular}:${contaCorrente2.saldo} ');
+  contaCorrente2.chequeEspecial = -200;
 
-  bool sucesso = contaCorrente1.saque(20);
-  double saldo = contaCorrente1.deposito(50);
-  print(sucesso);
-  print(saldo);
-  bool sucesso2 = contaCorrente1.saque(200);
-  print(sucesso2);
+  bool sucesso = contaCorrente1.transferencia(20, contaCorrente2);
 }
 
 class ContaCorrente {
@@ -26,13 +14,12 @@ class ContaCorrente {
   late int agencia = 145;
   late int conta;
   late double saldo = 20.0;
+  late double chequeEspecial = -100;
 
   bool saque(double valorDoSaque) {
-    if (saldo - valorDoSaque < -100) {
-      print('Sem saldo Suficiente');
+    if (!verificaSaldo(valorDoSaque)) {
       return false;
     } else {
-      print('Sacando $valorDoSaque reais');
       saldo -= valorDoSaque;
       return true;
     }
@@ -41,5 +28,26 @@ class ContaCorrente {
   double deposito(double valorDoDeposito) {
     saldo += valorDoDeposito;
     return saldo;
+  }
+
+  bool transferencia(
+      double valorDeTranferencia, ContaCorrente contaCorrenteDestino) {
+    if (!verificaSaldo(valorDeTranferencia)) {
+      return false;
+    } else {
+      saldo -= valorDeTranferencia;
+      contaCorrenteDestino.saldo += valorDeTranferencia;
+      return true;
+    }
+  }
+
+  bool verificaSaldo(double valor) {
+    if (saldo - valor < chequeEspecial) {
+      print('Sem saldo suficiente');
+      return false;
+    } else {
+      print('Movimentando $valor reais');
+      return true;
+    }
   }
 }
